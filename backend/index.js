b753d9766b76158e28ccab7360a1e8e37cb7e028
shashi-bhusan <shashi.bhusan.kumar@beenait.com>
2024-09-10 11:40:51 +0530

    Update PORT config for render

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(cors());
 app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 //connections
 mongoose
