3e086a4ecf008623f3da96a1015d9e76ab164904
alok182011 <alokkola21@gmail.com>
2024-11-18 20:20:27 +0530

    new features

--- backend/routes/revenue2.js
+++ backend/routes/revenue2.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { getMonthlyRevenueByPaymentMode } = require("../controllers/revenue2");
+const { getMonthlyRevenueByPaymentMode, getMonthlyRevenue } = require("../controllers/revenue2");
 
 const router = express.Router();
 
 router.get("/monthly-revenue", getMonthlyRevenueByPaymentMode);
+router.get("/new-revenue", getMonthlyRevenue);
 
 module.exports = router;
